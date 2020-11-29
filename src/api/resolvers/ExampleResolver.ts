cdf6abe551b107ca06403882b8085f8cbf3def02
Max Oehme <maxoehme@yahoo.de>
2020-11-29 16:04:15 +0100

    added graphql example resolver with type

--- src/api/resolvers/ExampleResolver.ts
+++ src/api/resolvers/ExampleResolver.ts
@@ -1,27 +1,32 @@
-import { Resolver, Mutation, Arg, Query } from "type-graphql";
+import {Arg, Mutation, Query, Resolver} from "type-graphql";
 import {Example} from "../types/Example"
+
+const exampleList: Example[] = [
+    new Example(1, "Herr der Ringe", "Lorem ipsum dolor sit amet"),
+    new Example(2, "Star Wars", "Metus aliquam eleifend "),
+    new Example(3, "Pretty Woman", "Vel eros donec ac odio tempor orci dapibus."),
+    new Example(4, "Mad Max", "Et netus et malesuada fames ac turpis egestas maecenas."),
+]
 
 @Resolver()
 export class ExampleResolver {
 
-    @Query(_returns => Example, { nullable: false})
-    async returnExample(@Arg("id") id: string){
-        const example = new Example();
-        example.name = "Max";
-        example.description = "Mustermann";
-        return example;
+    @Query(_returns => Example, {nullable: false})
+    async returnExample(@Arg("id") id: number) {
+        return exampleList.find(value => value.id == id);
     };
 
 
-    // @Query(() => [ExampleType])
-    // async returnAllExamples(){
-    //     return ;
-    // };
+    @Query(() => [Example])
+    async returnAllExamples() {
+        return exampleList;
+    };
 
-    // @Mutation(() => Boolean)
-    // async createCategory(@Arg("data"){name,description}: ExampleType) {
-    //
-    //     return true;
-    // };
+    @Mutation(() => Boolean)
+    async createCategory(@Arg("name") name: String, @Arg("description") description: String) {
+        const lastItem = exampleList.reduce((previousValue, currentValue) => previousValue.id < currentValue.id ? currentValue : previousValue);
+        exampleList.push(new Example(lastItem.id + 1, name, description));
+        return true;
+    };
 
 }
--- src/api/types/Example.ts
+++ src/api/types/Example.ts
@@ -3,8 +3,14 @@ import { ObjectType, Field, ID } from "type-graphql";
 @ObjectType({ description: "The Example model" })
 export class Example {
 
+    constructor(id: number, name: String, description: String) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
     @Field(()=> ID)
-    id: string;
+    id: number;
 
     @Field()
     name: String;
