b408407d79a8a96bbf242d37350668cf823d80fb
Max Oehme <maxoehme@yahoo.de>
2020-12-08 19:04:23 +0100

    NO_ISSUE added neo4j dependency and credentials

--- test/__utils/testServer.ts
+++ test/__utils/testServer.ts
@@ -4,9 +4,21 @@ import {
 } from "apollo-server-testing";
 import { ApolloServer } from "apollo-server-express";
 import createSchema from "../../src/schema";
+import * as dotenv from "dotenv";
+import * as neo4j from "neo4j-driver";
 
 export default async function testServer(): Promise<ApolloServerTestClient> {
+  dotenv.config();
+
+  const driver = neo4j.driver(
+    process.env.DATABASE_URL,
+    neo4j.auth.basic(
+      process.env.DATABASE_USERNAME,
+      process.env.DATABASE_PASSWORD
+    )
+  );
+
   const schema = await createSchema();
 
-  return createTestClient(new ApolloServer({ schema }));
+  return createTestClient(new ApolloServer({ schema, context: driver }));
 }
