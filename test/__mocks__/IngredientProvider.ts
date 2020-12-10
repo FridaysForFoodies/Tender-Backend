123c274d1fa626f2faaa10317743e4b4b84850c0
Luis Hankel <kaes3kuch3n@gmail.com>
2020-12-10 17:29:32 +0100

    TEN-41 Fixed IngredientProvider mock

--- test/__mocks__/IngredientProvider.ts
+++ test/__mocks__/IngredientProvider.ts
@@ -1,13 +1,16 @@
 import { Ingredient, Unit } from "../../src/model/Ingredient";
 import { IIngredientProvider } from "../../src/modules/ingredients/IngredientProvider";
 
+const ingredients = [
+  new Ingredient(0, "Flour", Unit.GRAMS, 364, 152),
+  new Ingredient(1, "Water", Unit.LITRES, 0, 25),
+  new Ingredient(2, "Egg", Unit.PIECES, 155, 395),
+  new Ingredient(3, "Salt", Unit.TEASPOONS, 0, 12),
+  new Ingredient(4, "Bell Pepper", Unit.PIECES, 20, 492),
+  new Ingredient(5, "Black Olives", Unit.GRAMS, 115, 125)
+]
+
 export const ingredientProviderMock = {
-  getAllWhere: jest.fn(() => new Promise((resolve) => resolve([
-    new Ingredient(0, "Flour", Unit.GRAMS, 364),
-    new Ingredient(1, "Water", Unit.LITRES, 0),
-    new Ingredient(2, "Egg", Unit.PIECES, 155),
-    new Ingredient(3, "Salt", Unit.TEASPOONS, 0),
-    new Ingredient(4, "Bell Pepper", Unit.PIECES, 20),
-    new Ingredient(5, "Black Olives", Unit.GRAMS, 115)
-  ])))
+  getAllWhere: jest.fn(() => new Promise((resolve) => resolve(ingredients))),
+  getPopular: jest.fn()
 } as IIngredientProvider;
