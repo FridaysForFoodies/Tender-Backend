26a2cef32c6421615eeda1690e1924c2bb7c0e52
Max Oehme <maxoehme@yahoo.de>
2020-12-14 21:42:20 +0100

    TEN-45 added setting to load local config

--- test/__utils/testServer.ts
+++ test/__utils/testServer.ts
@@ -5,9 +5,17 @@ import {
 import { ApolloServer } from "apollo-server-express";
 import createSchema from "../../src/schema";
 import * as dotenv from "dotenv";
+import * as path from "path";
 
 export default async function testServer(): Promise<ApolloServerTestClient> {
-  dotenv.config();
+  if (
+    process.env.DOTENV_CONFIG_LOCAL !== undefined &&
+    process.env.DOTENV_CONFIG_LOCAL
+  ) {
+    dotenv.config();
+  } else {
+    dotenv.config({ path: path.resolve(process.cwd(), ".env.test") });
+  }
 
   const schema = await createSchema();
 
