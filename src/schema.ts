b862e06c49eb0e671c4be53f81d2c785cae6fd59
Max Oehme <maxoehme@yahoo.de>
2020-12-18 11:42:45 +0100

    TEN-44 removed example

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,4 @@
 import { buildSchema } from "type-graphql";
-import { ExampleResolver } from "./modules/example/ExampleResolver";
 import { GraphQLSchema } from "graphql";
 import { IngredientResolver } from "./modules/ingredients/IngredientResolver";
 import { Container } from "typedi";
@@ -8,12 +7,7 @@ import { RecipePreferencesResolver } from "./modules/recipe_preferences/RecipePr
 
 export default async function createSchema(): Promise<GraphQLSchema> {
   return await buildSchema({
-    resolvers: [
-      ExampleResolver,
-      IngredientResolver,
-      UserResolver,
-      RecipePreferencesResolver,
-    ],
+    resolvers: [IngredientResolver, UserResolver, RecipePreferencesResolver],
     emitSchemaFile: true,
     validate: false,
     container: Container,
