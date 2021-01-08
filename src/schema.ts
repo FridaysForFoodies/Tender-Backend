fd27c3a4a36d66bb8a44c09dcb0747ff0870f8e4
Max Oehme <maxoehme@yahoo.de>
2021-01-08 12:18:49 +0100

    HotFix added tagresolver to scheme

--- src/schema.ts
+++ src/schema.ts
@@ -5,6 +5,7 @@ import { Container } from "typedi";
 import { UserResolver } from "./modules/user/UserResolver";
 import { RecipePreferencesResolver } from "./modules/recipe_preferences/RecipePreferencesResolver";
 import { RecipeResolver } from "./modules/recipe/RecipeResolver";
+import { TagResolver } from "./modules/tag/TagResolver";
 
 export default async function createSchema(): Promise<GraphQLSchema> {
   return await buildSchema({
@@ -13,6 +14,7 @@ export default async function createSchema(): Promise<GraphQLSchema> {
       UserResolver,
       RecipePreferencesResolver,
       RecipeResolver,
+      TagResolver,
     ],
     emitSchemaFile: true,
     validate: false,
